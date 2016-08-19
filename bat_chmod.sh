13efe72c4bec5ddf1ca25a5dc72f4498c98c4623
姜河 <jianghe@autoeeby.cn>
2016-08-19 13:20:24 +0800

    init

---
+++ bat_chmod.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+function scandir() {
+    local cur_dir parent_dir workdir
+    workdir=$1
+    cd ${workdir}
+    if [ ${workdir} = "/" ]
+    then
+        cur_dir=""
+    else
+        cur_dir=$(pwd)
+    fi
+
+    for dirlist in $(ls -A ${cur_dir})
+    do
+        if test -d ${dirlist};then
+            sudo chmod 775 ${dirlist}
+            cd ${dirlist}
+            scandir ${cur_dir}/${dirlist}
+            cd ..
+        else
+             tmpfile="${cur_dir}/${dirlist}"
+             sudo chmod 664 $tmpfile
+             
+             if [[ "`file $tmpfile`" =~ "ASCII" ]] ; then
+              echo -e "\033[33m `file $tmpfile` \033[0m"
+             elif [[ "`file $tmpfile`" =~ "UTF" ]] ; then
+              echo -e "\033[32m `file $tmpfile` \033[0m"
+             else 
+              echo -e "\033[31m `file $tmpfile` \033[0m"
+             fi
+        fi
+    done
+}
+
+if test -d $1
+then
+    scandir $1
+elif test -f $1
+then
+    echo "you input a file but not a directory,pls reinput and try again"
+    exit 1
+else
+    echo "the Directory isn't exist which you input,pls input a new one!!"
+    exit 1
+fi
+
+
